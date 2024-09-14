887d94e9b513f962c01060a5bb13a607a04d178f
nakayuu803 <ynakamura704@gmail.com>
2024-09-15 00:12:07 +0900

    ユーザー作成時DB作成

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -43,6 +43,25 @@ class _SignUpState extends State<SignUp> {
     Firebase.initializeApp();
   }
 
+  // 新規作成時Firestoreにユーザーデータを登録する関数
+  Future<void> _newUserCheckData(
+    User? user,
+    String? name,
+    String place,
+  ) async {
+    try {
+      await FirebaseFirestore.instance.collection('user').doc(user?.uid).set({
+        'name': name,
+        'place': place,
+        'check': List.generate(9, (_) => false),
+        'goods': List.generate(9, (_) => false),
+      });
+      print("ユーザーデータを登録したよ");
+    } catch (e) {
+      print("ユーザーデータの保存失敗！");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -165,6 +184,8 @@ class _SignUpState extends State<SignUp> {
                                   if (user != null)
                                     print(
                                         "ユーザ登録しました ${user.email} , ${user.uid}");
+                                  await _newUserCheckData(
+                                      user, user?.email, '沖縄県');
                                 } catch (e) {
                                   print(e);
                                 }
