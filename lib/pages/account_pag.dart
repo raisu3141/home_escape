7b710df7904933838e6e4d599db2c1903672e357
osaku039 <booklove039@gmail.com>
2024-09-15 03:06:14 +0900

    FireBaseからの取得まだ途中

--- lib/pages/account_pag.dart
+++ lib/pages/account_pag.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_escape/constant/constant.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -8,26 +10,62 @@ class AccountPage extends StatefulWidget {
   State<AccountPage> createState() => _AccountPageState();
 }
 
-class _AccountPageState extends State<AccountPage> {
+class _AccountPageState extends State<AccountPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
   String isSelectedValue = '沖縄県';
   List<String> choices = <String>[
-    '沖縄県', 
-    '三重県', 
+    '沖縄県',
+    '三重県',
     '愛知県',
     '北海道',
   ];
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _fetchChecklistData(); // Firestoreからデータを取得する
+  }
+
+  // Firestoreからチェックリストデータを取得する関数
+  Future<void> _fetchChecklistData() async {
+    try {
+      print("データ取得中...");
+      final uid = FirebaseAuth.instance.currentUser?.uid;
+      print(uid);
+      if (uid != null) {
+        final userDoc =
+            await FirebaseFirestore.instance.collection('user').doc(uid).get();
+        print(userDoc.data());
+
+        if (userDoc.exists) {
+          final checkData = List<bool>.from(
+              userDoc.data()?['check'] ?? List.filled(9, false));
+          final goodsData = List<bool>.from(
+              userDoc.data()?['goods'] ?? List.filled(11, false));
+
+          _loading = false; // データ取得完了
+        }
+        ;
+      }
+    } catch (e) {
+      print("データ取得エラー: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'アカウント',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Color(0xFFF38D49),
+          centerTitle: true,
+          title: const Text(
+            'アカウント',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color(0xFFF38D49),
         ),
         body: SingleChildScrollView(
           child: Center(
@@ -35,18 +73,14 @@ class _AccountPageState extends State<AccountPage> {
               children: [
                 SizedBox(height: Constant.deviceHeight * 0.06),
                 const Icon(
-                    IconData(0xee35, fontFamily: 'MaterialIcons'),
-                    color: Color(0xFFF38D49),
-                    size: 150,
-                  ),
+                  IconData(0xee35, fontFamily: 'MaterialIcons'),
+                  color: Color(0xFFF38D49),
+                  size: 150,
+                ),
                 const Padding(
                   padding: EdgeInsets.all(8.0),
-                  child:  Text(
-                    'よしにき',
-                    style: TextStyle(fontSize: 30)
-                  ),
+                  child: Text('よしにき', style: TextStyle(fontSize: 30)),
                 ),
-          
                 SizedBox(height: Constant.deviceHeight * 0.17),
                 showEmail(),
                 SizedBox(height: Constant.deviceHeight * 0.03),
@@ -60,26 +94,22 @@ class _AccountPageState extends State<AccountPage> {
     );
   }
 
-  Widget showEmail(){
+  Widget showEmail() {
     return Column(
       children: [
         Row(
           children: [
             SizedBox(width: Constant.deviceWidth * 0.13),
-            const Text(
-              'mail',
-              style: TextStyle(
-                fontSize: 20,
-              )
-            ),
+            const Text('mail',
+                style: TextStyle(
+                  fontSize: 20,
+                )),
           ],
         ),
         const SizedBox(height: 10),
         const Text(
           'motoki@gmail.com',
-          style: TextStyle(
-            fontSize: 20
-          ),
+          style: TextStyle(fontSize: 20),
         ),
         const SizedBox(height: 10),
         const Divider(
@@ -89,18 +119,16 @@ class _AccountPageState extends State<AccountPage> {
     );
   }
 
-  Widget showLocation(){
+  Widget showLocation() {
     return Column(
       children: [
         Row(
           children: [
             SizedBox(width: Constant.deviceWidth * 0.13),
-            const Text(
-              '所在地',
-              style: TextStyle(
-                fontSize: 20,
-              )
-            ),
+            const Text('所在地',
+                style: TextStyle(
+                  fontSize: 20,
+                )),
           ],
         ),
         const SizedBox(height: 10),
@@ -113,10 +141,10 @@ class _AccountPageState extends State<AccountPage> {
     );
   }
 
-  Widget pullDown(){
+  Widget pullDown() {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color:const Color(0xFF040404)),
+        border: Border.all(color: const Color(0xFF040404)),
         borderRadius: const BorderRadius.all(Radius.circular(10)),
       ),
       padding: const EdgeInsets.fromLTRB(100, 2, 100, 2),
