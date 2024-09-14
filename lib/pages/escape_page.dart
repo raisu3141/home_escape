1d1b3b8fb2f7d37fe68ee659dc97a7f3e3432d60
nakayuu803 <ynakamura704@gmail.com>
2024-09-14 15:55:45 +0900

    escapeUI整え

--- lib/pages/escape_page.dart
+++ lib/pages/escape_page.dart
@@ -8,6 +8,8 @@ class EscapePage extends StatelessWidget {
     final double deviceHeight =
         MediaQuery.of(context).size.height; //端末の縦の大きさを取得
     final double deviceWidth = MediaQuery.of(context).size.width; //端末の横の大きさを取得
+    final double horizontalPadding = deviceWidth * 0.1;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -17,53 +19,69 @@ class EscapePage extends StatelessWidget {
         ),
         backgroundColor: const Color(0xFFF38D49),
       ),
-      body: const Column(
-        children: [
-          Text(
-            '落ち着いて以下の行動をとってください',
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 20),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '1.',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.orange,
-                    fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+        child: const Column(
+          children: [
+            SizedBox(height: 30),
+            Text(
+              '落ち着いて以下の行動をとってください',
+              style: TextStyle(fontSize: 17),
+            ),
+            SizedBox(height: 40),
+            SizedBox(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '1.',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Text(
+                      '机の下に隠れてください',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(width: 10),
-              Expanded(
-                child: Text(
-                  '机の下に隠れてください',
-                  style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '2.',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.orange,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '2.',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.orange,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(width: 10),
-              Expanded(
-                child: Text(
-                  '屋外にいる場合は建物に近づかないでください',
-                  style: TextStyle(fontSize: 18),
+                SizedBox(
+                  width: 10,
                 ),
-              ),
-            ],
-          ),
-        ],
+                Expanded(
+                  child: Text(
+                    '屋外にいる場合は建物に近づかないでください',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
