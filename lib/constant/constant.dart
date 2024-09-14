4034269969d9b93efc72f1a84971babb76dbe10a
osaku039 <booklove039@gmail.com>
2024-09-14 17:15:18 +0900

    checkpageとカラーコード

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,3 +1,6 @@
 class Constant {
-  static const String buttonColor = '0xFFF38D49';
+  static const int mainColor = 0xFFF38D49;
+  static const int mainFontColor = 0xFF404040;
+  static const int accentColor = 0xFF89C58B;
+  static const int backGroundColor = 0xFFFFFAF2;
 }
